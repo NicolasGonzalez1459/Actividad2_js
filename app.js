28a1b5c86239127ed77d0b3d8b13248aac0e4cf5
NicolasGonzalez1459 <nicolas6403@gmail.com>
2026-05-29 20:35:52 -0300

    feat(seg parte):Agrego segunda parte

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Parte 1
 const in1=document.getElementById("in1");
 const btn1=document.getElementById("btn1");
 const p1=document.getElementById("p1");
@@ -14,5 +15,23 @@ btn1.addEventListener("click",function(){
     else{
         p1.style.color="red";
         p1.textContent="El campo no puede tener más de 3 caracteres";
+    }
+});
+
+//Parte 2
+const categorias = ["Accion", "Aventura", "Comedia", "Drama", "Fantasia", "Terror"];
+const btn2 =document.getElementById("btn2");
+const btn3 =document.getElementById("btn3");
+const ul1 = document.getElementById("ul1");
+
+btn2.addEventListener("click", function(){
+    for (let i = 0; i < categorias.length; i++){
+        ul1.innerHTML += "<li>" + categorias[i] + "</li>";
+    }
+});
+
+btn3.addEventListener("click", function(){
+    for (let i=0; i<3; i++){
+        ul1.innerHTML += "<li>" + categorias[i] + "</li>";
     }
 });
